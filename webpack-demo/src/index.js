23242388d3a595ae2892bedec268c33e0e5d6d9c
Fondem Junior <fondemjunior@yahoo.com>
2021-10-27 11:03:49 +0100

    Add function to iterate the array of objects

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,19 +2,37 @@ import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
+// function component() {
+//     const element = document.createElement('div');
+//     const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+//     btn.innerHTML = 'Click me and check the console!';
+//     btn.onclick = printMe;
 
-    element.appendChild(btn);
-    return element;
-  }
+//     element.appendChild(btn);
+//     return element;
+//   }
+
+
   
-  document.body.appendChild(component());
+//   document.body.appendChild(component());
+
+const todo = [
+  { description: 'Set up a new project with webpack', isCompleted: false, index: 0 },
+  { description: 'Set up a new project with webpack', isCompleted: false, index: 1 },
+  { description: 'Create an index.js file', isCompleted: false, index: 2 },
+  { description: 'Write a function to iterate over the tasks array and populate an HTML', isCompleted: false, index: 3 },
+];
+
+function todoList(){
+  let todoListContent = '';
+  todo.forEach((item) =>{
+    todoListContent += `<li class="task"><input class="task-check" type="checkbox"><span>${item.description}</span></li>`;
+  });
+  document.querySelector('.todo-list').innerHTML = todoListContent;
+}
+todoList();
