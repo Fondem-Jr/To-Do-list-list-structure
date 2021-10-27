820f10892ebce34449018e87ccc461389b4858f9
Fondem Junior <fondemjunior@yahoo.com>
2021-10-27 17:50:42 +0100

    fix linters

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+/* eslint-disable*/
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ /* eslint-enable */ 
 module.exports = {
   entry: {
     index: './src/index.js',
