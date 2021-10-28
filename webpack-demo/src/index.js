0792de2c21089d68d3548cf12652400f9b7ee26a
Fondem Junior <fondemjunior@yahoo.com>
2021-10-28 21:08:22 +0100

    Make the the list interactive

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,7 @@
-// import _ from 'lodash';
-// import printMe from './print.js';
+import * as task from './status.js';
 import './style.css';
 
-const todo = [
+let list = [
   { description: 'Set up a new project with webpack', isCompleted: false, index: 0 },
   { description: 'Set up a new project with webpack', isCompleted: false, index: 1 },
   { description: 'Create an index.js file', isCompleted: false, index: 2 },
@@ -10,10 +9,51 @@ const todo = [
 ];
 
 function todoList() {
-  let todoListContent = '';
-  todo.forEach((item) => {
-    todoListContent += `<li class="task"><input class="task-check" type="checkbox"><span class="list">${item.description}</span><i class='fa fa-ellipsis-v' style="margin-left:auto"></i></li>`;
+  if (window.localStorage.getItem('localTasks')) {
+    const localTasks = window.localStorage.getItem('localTasks');
+    list = JSON.parse(localTasks);
+  }
+  document.querySelector('.todo-list').innerHTML = '';
+  list.forEach((item) => {
+    const taskElement = document.createElement('li');
+    taskElement.classList.add('task');
+    if (item.isCompleted) {
+      taskElement.classList.add('completed');
+    }
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-check');
+    checkbox.addEventListener('click', () => {
+      task.status(item, list);
+      todoList();
+    });
+    checkbox.checked = item.isCompleted;
+    taskElement.appendChild(checkbox);
+    const taskText = document.createElement('input');
+    taskText.classList = 'task-text';
+    taskText.value = item.description;
+    taskText.addEventListener('change', () => {
+      if (taskText.value.length > 0) {
+        item.description = taskText.value;
+        task.saveLocal(list);
+      }
+    });
+    taskElement.appendChild(taskText);
+    const dragIcon = document.createElement('i');
+    dragIcon.classList = 'fas fa-ellipsis-v drag icon';
+    taskElement.appendChild(dragIcon);
+    taskElement.draggable = 'true';
+    document.querySelector('.todo-list').appendChild(taskElement);
   });
-  document.querySelector('.todo-list').innerHTML = todoListContent;
 }
+
 todoList();
+document.querySelector('#taskForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  task.add(list);
+  todoList();
+});
+document.querySelector('.clearer').addEventListener('click', () => {
+  task.removeDone(list);
+  todoList();
+});
