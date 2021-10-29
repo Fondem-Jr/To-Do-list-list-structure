b7d69963365a56cc6558c812aef5ec093fd4f25d
Fondem Junior <fondemjunior@yahoo.com>
2021-10-30 00:11:39 +0100

    fix matching iDs with property index and fix the delete button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as stat from './status.js';
 import './style.css';
 
 let list = [];
+const listEl = document.querySelector('ul');
 
 function todoList() {
   if (window.localStorage.getItem('localTasks')) {
@@ -35,14 +36,16 @@ function todoList() {
       }
     });
     taskElement.appendChild(taskText);
-    const dragIcon = document.createElement('i');
-    dragIcon.classList = 'fas fa-ellipsis-v drag icon';
+    
+    const dragIcon = document.createElement('button');
+    dragIcon.classList = 'far fa-trash-alt deleteBtn';
     taskElement.appendChild(dragIcon);
     taskElement.draggable = 'true';
     document.querySelector('.todo-list').appendChild(taskElement);
-  });
+  });  
 }
 
+listEl.addEventListener('click', removeItem);
 todoList();
 document.querySelector('#taskForm').addEventListener('submit', (event) => {
   event.preventDefault();
@@ -53,3 +56,19 @@ document.querySelector('.clearer').addEventListener('click', () => {
   task.removeDone(list);
   todoList();
 });
+
+function removeItem(e) { 
+  if (!e.target.classList.contains('deleteBtn')){
+    return;
+  }
+  const btn = e.target;
+  list.forEach((task) => {
+    if(task.description === btn.parentElement.children[1].value){
+      list.splice(list.indexOf(task), 1);
+    }
+  });
+  console.table(btn.parentElement.children);
+  btn.closest("li").remove();
+  task.updateIndex(list);
+  stat.saveLocal(list);
+ }
--- src/populate.js
+++ src/populate.js
@@ -1,13 +1,13 @@
 import { saveLocal } from './status.js';
 
 export function add(list) {
-  list.push({ description: document.querySelector('#newTask').value, isCompleted: false, index: list.length });
+  list.push({ description: document.querySelector('#newTask').value, isCompleted: false, index: list.length + 1});
   document.querySelector('#newTask').value = '';
   saveLocal(list);
 }
 
 export function updateIndex(list) {
-  let i = 0;
+  let i = 1;
   list.forEach((elem) => {
     elem.index = i;
     i += 1;
